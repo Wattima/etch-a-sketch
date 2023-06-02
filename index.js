c724230f7ad120a4716bc5ccfc74ee1b7adaea34
Wattima <darwattima@gmail.com>
2023-06-02 12:13:16 +0300

    prompted user cells

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const container = document.getElementsByClassName("container")[0];
 const grid = document.createElement("div");
 grid.setAttribute("id", "grid");
 // grid.textContent = "Should be grid";
+let cells = 16;
+
+
 
 grid.style.display = 'grid';
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
+grid.style.gridTemplateColumns = `repeat(${cells}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${cells}, 1fr)`;
 
 
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < cells; i++) {
+    for (let j = 0; j < cells; j++) {
       const gridCell = document.createElement("div");
       gridCell.classList.add("grid-cell");
       grid.appendChild(gridCell);
     }
   }
+
+cells = prompt("Enter number of cells");
 
 container.appendChild(grid);
